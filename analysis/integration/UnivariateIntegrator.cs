5f10ab3501360671f6b1878e8ad7eb91c8d077ce
Oleksandr M. Kornieiev <onekosha@gmail.com>
2014-09-17 12:53:39 +0300

    UnivariateIntegrator
    Change for UnivariateIntegrator.cs. Change only namespace

--- analysis/integration/UnivariateIntegrator.cs
+++ analysis/integration/UnivariateIntegrator.cs
@@ -14,75 +14,75 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-namespace org.apache.commons.math3.analysis.integration
+namespace mathlib.integration
 {
 
-	using MathIllegalArgumentException = org.apache.commons.math3.exception.MathIllegalArgumentException;
-	using MaxCountExceededException = org.apache.commons.math3.exception.MaxCountExceededException;
-	using NullArgumentException = org.apache.commons.math3.exception.NullArgumentException;
-	using TooManyEvaluationsException = org.apache.commons.math3.exception.TooManyEvaluationsException;
+    using MathIllegalArgumentException = mathlib.exception.MathIllegalArgumentException;
+    using MaxCountExceededException = mathlib.exception.MaxCountExceededException;
+    using NullArgumentException = mathlib.exception.NullArgumentException;
+    using TooManyEvaluationsException = mathlib.exception.TooManyEvaluationsException;
 
-	/// <summary>
-	/// Interface for univariate real integration algorithms.
-	/// 
-	/// @version $Id: UnivariateIntegrator.java 1364387 2012-07-22 18:14:11Z tn $
-	/// @since 1.2
-	/// </summary>
-	public interface UnivariateIntegrator
-	{
+    /// <summary>
+    /// Interface for univariate real integration algorithms.
+    /// 
+    /// @version $Id: UnivariateIntegrator.java 1364387 2012-07-22 18:14:11Z tn $
+    /// @since 1.2
+    /// </summary>
+    public interface UnivariateIntegrator
+    {
 
-		/// <summary>
-		/// Get the actual relative accuracy. </summary>
-		/// <returns> the accuracy </returns>
-		double RelativeAccuracy {get;}
+        /// <summary>
+        /// Get the actual relative accuracy. </summary>
+        /// <returns> the accuracy </returns>
+        double RelativeAccuracy { get; }
 
-		/// <summary>
-		/// Get the actual absolute accuracy.
-		/// </summary>
-		/// <returns> the accuracy </returns>
-		double AbsoluteAccuracy {get;}
+        /// <summary>
+        /// Get the actual absolute accuracy.
+        /// </summary>
+        /// <returns> the accuracy </returns>
+        double AbsoluteAccuracy { get; }
 
-		/// <summary>
-		/// Get the min limit for the number of iterations.
-		/// </summary>
-		/// <returns> the actual min limit </returns>
-		int MinimalIterationCount {get;}
+        /// <summary>
+        /// Get the min limit for the number of iterations.
+        /// </summary>
+        /// <returns> the actual min limit </returns>
+        int MinimalIterationCount { get; }
 
-		/// <summary>
-		/// Get the upper limit for the number of iterations.
-		/// </summary>
-		/// <returns> the actual upper limit </returns>
-		int MaximalIterationCount {get;}
+        /// <summary>
+        /// Get the upper limit for the number of iterations.
+        /// </summary>
+        /// <returns> the actual upper limit </returns>
+        int MaximalIterationCount { get; }
 
-		/// <summary>
-		/// Integrate the function in the given interval.
-		/// </summary>
-		/// <param name="maxEval"> Maximum number of evaluations. </param>
-		/// <param name="f"> the integrand function </param>
-		/// <param name="min"> the min bound for the interval </param>
-		/// <param name="max"> the upper bound for the interval </param>
-		/// <returns> the value of integral </returns>
-		/// <exception cref="TooManyEvaluationsException"> if the maximum number of function
-		/// evaluations is exceeded. </exception>
-		/// <exception cref="MaxCountExceededException"> if the maximum iteration count is exceeded
-		/// or the integrator detects convergence problems otherwise </exception>
-		/// <exception cref="MathIllegalArgumentException"> if min > max or the endpoints do not
-		/// satisfy the requirements specified by the integrator </exception>
-		/// <exception cref="NullArgumentException"> if {@code f} is {@code null}. </exception>
-//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-//ORIGINAL LINE: double integrate(int maxEval, org.apache.commons.math3.analysis.UnivariateFunction f, double min, double max) throws org.apache.commons.math3.exception.TooManyEvaluationsException, org.apache.commons.math3.exception.MaxCountExceededException, org.apache.commons.math3.exception.MathIllegalArgumentException, org.apache.commons.math3.exception.NullArgumentException;
-		double integrate(int maxEval, UnivariateFunction f, double min, double max);
+        /// <summary>
+        /// Integrate the function in the given interval.
+        /// </summary>
+        /// <param name="maxEval"> Maximum number of evaluations. </param>
+        /// <param name="f"> the integrand function </param>
+        /// <param name="min"> the min bound for the interval </param>
+        /// <param name="max"> the upper bound for the interval </param>
+        /// <returns> the value of integral </returns>
+        /// <exception cref="TooManyEvaluationsException"> if the maximum number of function
+        /// evaluations is exceeded. </exception>
+        /// <exception cref="MaxCountExceededException"> if the maximum iteration count is exceeded
+        /// or the integrator detects convergence problems otherwise </exception>
+        /// <exception cref="MathIllegalArgumentException"> if min > max or the endpoints do not
+        /// satisfy the requirements specified by the integrator </exception>
+        /// <exception cref="NullArgumentException"> if {@code f} is {@code null}. </exception>
+        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
+        //ORIGINAL LINE: double integrate(int maxEval, org.apache.commons.math3.analysis.UnivariateFunction f, double min, double max) throws org.apache.commons.math3.exception.TooManyEvaluationsException, org.apache.commons.math3.exception.MaxCountExceededException, org.apache.commons.math3.exception.MathIllegalArgumentException, org.apache.commons.math3.exception.NullArgumentException;
+        double integrate(int maxEval, UnivariateFunction f, double min, double max);
 
-		/// <summary>
-		/// Get the number of function evaluations of the last run of the integrator. </summary>
-		/// <returns> number of function evaluations </returns>
-		int Evaluations {get;}
+        /// <summary>
+        /// Get the number of function evaluations of the last run of the integrator. </summary>
+        /// <returns> number of function evaluations </returns>
+        int Evaluations { get; }
 
-		/// <summary>
-		/// Get the number of iterations of the last run of the integrator. </summary>
-		/// <returns> number of iterations </returns>
-		int Iterations {get;}
+        /// <summary>
+        /// Get the number of iterations of the last run of the integrator. </summary>
+        /// <returns> number of iterations </returns>
+        int Iterations { get; }
 
-	}
+    }
 
 }
