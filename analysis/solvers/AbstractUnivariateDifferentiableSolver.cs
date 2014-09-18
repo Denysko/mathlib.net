d8074273bedb9338f9e12d412f37f896fa91956e
Oleksandr M. Kornieiev <onekosha@gmail.com>
2014-09-18 16:02:57 +0300

    AbstractUnive
    Change namespace

--- analysis/solvers/AbstractUnivariateDifferentiableSolver.cs
+++ analysis/solvers/AbstractUnivariateDifferentiableSolver.cs
@@ -15,72 +15,74 @@
  * limitations under the License.
  */
 
-namespace org.apache.commons.math3.analysis.solvers
+namespace mathlib.analysis.solvers
 {
 
-	using DerivativeStructure = org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
-	using UnivariateDifferentiableFunction = org.apache.commons.math3.analysis.differentiation.UnivariateDifferentiableFunction;
-	using TooManyEvaluationsException = org.apache.commons.math3.exception.TooManyEvaluationsException;
+    using DerivativeStructure = mathlib.analysis.differentiation.DerivativeStructure;
+    using UnivariateDifferentiableFunction = mathlib.analysis.differentiation.UnivariateDifferentiableFunction;
+    using TooManyEvaluationsException = mathlib.exception.TooManyEvaluationsException;
 
-	/// <summary>
-	/// Provide a default implementation for several functions useful to generic
-	/// solvers.
-	/// 
-	/// @since 3.1
-	/// @version $Id: AbstractUnivariateDifferentiableSolver.java 1455194 2013-03-11 15:45:54Z luc $
-	/// </summary>
-	public abstract class AbstractUnivariateDifferentiableSolver : BaseAbstractUnivariateSolver<UnivariateDifferentiableFunction>, UnivariateDifferentiableSolver
-	{
+    /// <summary>
+    /// Provide a default implementation for several functions useful to generic
+    /// solvers.
+    /// 
+    /// @since 3.1
+    /// @version $Id: AbstractUnivariateDifferentiableSolver.java 1455194 2013-03-11 15:45:54Z luc $
+    /// </summary>
+    public abstract class AbstractUnivariateDifferentiableSolver : BaseAbstractUnivariateSolver<UnivariateDifferentiableFunction>, UnivariateDifferentiableSolver
+    {
 
-		/// <summary>
-		/// Function to solve. </summary>
-		private UnivariateDifferentiableFunction function;
+        /// <summary>
+        /// Function to solve. </summary>
+        private UnivariateDifferentiableFunction function;
 
-		/// <summary>
-		/// Construct a solver with given absolute accuracy.
-		/// </summary>
-		/// <param name="absoluteAccuracy"> Maximum absolute error. </param>
-//JAVA TO C# CONVERTER WARNING: 'final' parameters are not allowed in .NET:
-//ORIGINAL LINE: protected AbstractUnivariateDifferentiableSolver(final double absoluteAccuracy)
-		protected internal AbstractUnivariateDifferentiableSolver(double absoluteAccuracy) : base(absoluteAccuracy)
-		{
-		}
+        /// <summary>
+        /// Construct a solver with given absolute accuracy.
+        /// </summary>
+        /// <param name="absoluteAccuracy"> Maximum absolute error. </param>
+        //JAVA TO C# CONVERTER WARNING: 'final' parameters are not allowed in .NET:
+        //ORIGINAL LINE: protected AbstractUnivariateDifferentiableSolver(final double absoluteAccuracy)
+        protected internal AbstractUnivariateDifferentiableSolver(double absoluteAccuracy)
+            : base(absoluteAccuracy)
+        {
+        }
 
-		/// <summary>
-		/// Construct a solver with given accuracies.
-		/// </summary>
-		/// <param name="relativeAccuracy"> Maximum relative error. </param>
-		/// <param name="absoluteAccuracy"> Maximum absolute error. </param>
-		/// <param name="functionValueAccuracy"> Maximum function value error. </param>
-//JAVA TO C# CONVERTER WARNING: 'final' parameters are not allowed in .NET:
-//ORIGINAL LINE: protected AbstractUnivariateDifferentiableSolver(final double relativeAccuracy, final double absoluteAccuracy, final double functionValueAccuracy)
-		protected internal AbstractUnivariateDifferentiableSolver(double relativeAccuracy, double absoluteAccuracy, double functionValueAccuracy) : base(relativeAccuracy, absoluteAccuracy, functionValueAccuracy)
-		{
-		}
+        /// <summary>
+        /// Construct a solver with given accuracies.
+        /// </summary>
+        /// <param name="relativeAccuracy"> Maximum relative error. </param>
+        /// <param name="absoluteAccuracy"> Maximum absolute error. </param>
+        /// <param name="functionValueAccuracy"> Maximum function value error. </param>
+        //JAVA TO C# CONVERTER WARNING: 'final' parameters are not allowed in .NET:
+        //ORIGINAL LINE: protected AbstractUnivariateDifferentiableSolver(final double relativeAccuracy, final double absoluteAccuracy, final double functionValueAccuracy)
+        protected internal AbstractUnivariateDifferentiableSolver(double relativeAccuracy, double absoluteAccuracy, double functionValueAccuracy)
+            : base(relativeAccuracy, absoluteAccuracy, functionValueAccuracy)
+        {
+        }
 
-		/// <summary>
-		/// Compute the objective function value.
-		/// </summary>
-		/// <param name="point"> Point at which the objective function must be evaluated. </param>
-		/// <returns> the objective function value and derivative at specified point. </returns>
-		/// <exception cref="TooManyEvaluationsException">
-		/// if the maximal number of evaluations is exceeded. </exception>
-//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
-//ORIGINAL LINE: protected org.apache.commons.math3.analysis.differentiation.DerivativeStructure computeObjectiveValueAndDerivative(double point) throws org.apache.commons.math3.exception.TooManyEvaluationsException
-		protected internal virtual DerivativeStructure computeObjectiveValueAndDerivative(double point)
-		{
-			incrementEvaluationCount();
-			return function.value(new DerivativeStructure(1, 1, 0, point));
-		}
+        /// <summary>
+        /// Compute the objective function value.
+        /// </summary>
+        /// <param name="point"> Point at which the objective function must be evaluated. </param>
+        /// <returns> the objective function value and derivative at specified point. </returns>
+        /// <exception cref="TooManyEvaluationsException">
+        /// if the maximal number of evaluations is exceeded. </exception>
+        //JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
+        //ORIGINAL LINE: protected org.apache.commons.math3.analysis.differentiation.DerivativeStructure computeObjectiveValueAndDerivative(double point) throws org.apache.commons.math3.exception.TooManyEvaluationsException
+        protected internal virtual DerivativeStructure computeObjectiveValueAndDerivative(double point)
+        {
+            incrementEvaluationCount();
+            return function.value(new DerivativeStructure(1, 1, 0, point));
+        }
 
-		/// <summary>
-		/// {@inheritDoc}
-		/// </summary>
-		protected internal override void setup(int maxEval, UnivariateDifferentiableFunction f, double min, double max, double startValue)
-		{
-			base.setup(maxEval, f, min, max, startValue);
-			function = f;
-		}
-	}
+        /// <summary>
+        /// {@inheritDoc}
+        /// </summary>
+        protected internal override void setup(int maxEval, UnivariateDifferentiableFunction f, double min, double max, double startValue)
+        {
+            base.setup(maxEval, f, min, max, startValue);
+            function = f;
+        }
+    }
 
 }
